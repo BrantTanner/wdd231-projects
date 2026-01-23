7b2b451723a563b49ee02d1d8b7e6dcd833fe7b4
BrantTanner <brant.tanner123@gmail.com>
2026-01-23 14:28:13 -0700

    nps part 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,9 @@
-import { getParkData } from "./parkService.mjs";
-import { getParkInfoLinks } from "./parkService.mjs";
+import { getParkData, getParkInfoLinks } from "./parkService.mjs";
 
 import { getMediaCardTemplate } from "./templates.mjs";
 
 import { getHeaderFooter } from "./setHeaderFooter.mjs";
-
-const parkData = getParkData();
-const parkInfoLinks = getParkInfoLinks();
+import {getInfoLinks} from "./parkService.mjs"
 
 
 
@@ -18,10 +15,17 @@ function setIntroInfo(data){
     description.textContent = data.description
 
     const media_card = document.querySelector("#info > .media-card")
-    media_card.innerHTML = getMediaCardTemplate(parkInfoLinks)
+    media_card.innerHTML = getMediaCardTemplate(getParkInfoLinks())
     
 }
 
-setIntroInfo(parkData);
+async function init(){
+    const parkData = await getParkData();
+    const links = getInfoLinks(parkData.images);
 
-getHeaderFooter(parkData);
+    setIntroInfo(parkData);
+    getHeaderFooter(parkData);
+}
+
+init()
+
